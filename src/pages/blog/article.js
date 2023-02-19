fbedbcd4731aeaa1e2fa5d165acef29c44ba7baa
Grégory Marini <greg.marini@hotmail.fr>
2023-02-19 17:41:48 +0100

    premier contact avec nextjs

---
+++ src/pages/blog/article.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function article() {
+    return (
+        <div>
+            <p>J'écris mon premier article</p>
+        </div>
+    );
+}
---
+++ src/pages/blog/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function index() {
+    return (
+        <div>
+            <h1>Mon blog</h1>
+        </div>
+    );
+}
+
+export default index;
