01aae00d87ffe0e06552f14ee8bfaafbab5c86e4
Grégory Marini <greg.marini@hotmail.fr>
2023-02-19 18:49:52 +0100

    learn about navigation in nexjs

---
+++ src/pages/blog/[...slug].js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function article() {
+    return (
+        <div>
+            <p>Pour apprendre next js et c'est cool</p>
+        </div>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import { Inter } from "@next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+    const id = "article";
     return (
         <>
             <h1>Hello Next JS</h1>
+            <a href="https://google.com">google</a>
+            <Link href={`/blog/${id}`}>Blog</Link>
         </>
     );
 }
